7507ca6778e1d6118773a9fac8759f877ad9b3b2
Sarah Soutoul <sarahsoutoul@Sarahs-MacBook-Air.local>
2024-10-31 19:22:04 -0600

    fix images

--- src/components/Logo/index.jsx
+++ src/components/Logo/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import Logo from '../../assets/logo.svg'
+import LogoIMG from '../../assets/logo.svg'
 
 export default function Logo() {
   return (
-    <img id="logo" src={Logo} />
+    <img id="logo" src={LogoIMG} />
   )
 }
